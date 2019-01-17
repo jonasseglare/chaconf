4645a3be032ab9a90abfc9c38b12ec850d5b186f
Jonas Östlund <uppfinnarjonas@gmail.com>
2019-01-17 13:14:31 +0100

    good so far

--- src/chaconf/parser.clj
+++ src/chaconf/parser.clj
@@ -2,30 +2,79 @@
   (:require [clojure.string :as cljstr]))
 
 (def re-section #"^\s*(\S+)(\s+(\S.*))?:\s*$")
-(def re-blank #"^\s*$")
-(def re-count #"^\s*(\S.*)\s+(\d+)$")
-
-(defn parse-blank [x]
-  (re-matches re-blank x))
+(def re-count #"^\s*(\S.*)\s+(\d+)\s*$")
 
 (defn parse-section [x]
   (if-let [[full section-tag _ section-name] (re-matches
                                               re-section
                                               x)]
-    {:type (cljstr/trim section-tag)
-     :name (cljstr/trim section-name)}))
+    (merge
+     {:type (cljstr/trim section-tag)}
+     (if section-name
+       {:name (cljstr/trim section-name)}
+       {}))))
 
 (defn parse-count [x]
-  (if-let [[key n] (re-matches re-count x)]
+  (if-let [[_ key n] (re-matches re-count x)]
     {:key (cljstr/trim key)
      :count (read-string n)}))
 
-(defn split-and-enumarate-lines [src]
+(defn split-and-enumerate-lines [src]
   (let [lines (cljstr/split-lines src)]
     (map (fn [index line] {:index (inc index)
                            :line line})
          (range (count lines))
          lines)))
+
+(defn drop-blanks [numbered-lines]
+  (drop-while (comp cljstr/blank? :line) numbered-lines))
+
+(def section-headers {"session" :sessions
+                      "ensemble" :ensembles})
+
+(defn add-at-key [dst k x]
+  (update dst k (fn [dst] (conj (or dst []) x))))
+
+(defn read-counts [src]
+  (take-while (complement nil?)
+              (map (comp parse-count :line) src)))
+
+;; (read-counts [{:line "   Violin  4 " :index 8}])
+
+(defn read-section [input]
+  (println "Read input from" input)
+  (let [numbered-lines (drop-blanks (:numbered-lines input))]
+    (if (not (empty? numbered-lines))
+      (let [[header-line & numbered-lines] numbered-lines]
+        (if-let [header (parse-section (:line header-line))]
+          (let [header-type (cljstr/lower-case (:type header))
+                k (get section-headers header-type)]
+            (if (nil? k)
+              (merge {:error :section-not-recognized} header)
+              (let [counts (read-counts numbered-lines)]
+                (-> input
+                    (add-at-key k counts)
+                    (assoc :numbered-lines (drop (count counts)
+                                                 numbered-lines))))))
+          (merge header-line {:error :failed-to-parse}))))))
+
+;; (read-section {:numbered-lines [{:index 9 :line "asdf"}]})
+;; (read-section {:numbered-lines [{:index 9 :line "asdf:"}]})
+;; (read-section {:numbered-lines [{:index 9 :line "session:"}]})
+
+(defn read-sections [numbered-lines]
+  (let [result
+        (last
+         (take-while
+          (complement nil?)
+          (iterate read-section
+                   {:numbered-lines numbered-lines})))]
+    result))
+
+(defn parse [s]
+  (-> s
+      split-and-enumarate-lines
+      read-sections))
 
 (comment
 
@@ -52,7 +101,9 @@
                     "Cello 1  "
                     ]))
     
-    (split-and-enumarate-lines test-data)
+    (def d  (-> test-data
+                split-and-enumarate-lines
+                read-sections))
 
 
     
--- test/chaconf/parser_test.clj
+++ test/chaconf/parser_test.clj
@@ -1,6 +1,7 @@
 (ns chaconf.parser-test
   (:require [chaconf.parser :refer :all]
-            [clojure.test :refer :all]))
+            [clojure.test :refer :all]
+            [clojure.string :as cljstr]))
 
 (deftest regex-test
   (is (= "Sec" (second (re-matches re-section "    Sec:"))))
@@ -8,6 +9,39 @@
   (is (nil? (re-matches re-section "asdfadsf")))
   (is (= {:type "Section"
           :name "Mjao"}
-         (parse-section "   Section    Mjao :  "))))
+         (parse-section "   Section    Mjao :  ")))
+  (is (nil? (parse-section "asdfsafd")))
+
+)
 
 
+(def test-data2
+  (cljstr/join "\n"
+               [""
+                ""
+                ""
+                " Session: "
+                " Violin 20  "
+                "Cello 12"
+                "Alto 5"
+                "Piano 3"
+                "\n"
+                "\n"
+                "Ensemble V+V: "
+                "Violin 2"
+                "\n"
+                "Ensemble V+A :"
+                "Violin 1"
+                "Cello 1  "
+                ]))
+
+(deftest parse-test
+  (let [result (parse test-data2)]
+    (is (not (contains? result :error)))
+    (is (= 1 (count (:sessions result))))
+    (is (= 2 (count (:ensembles result)))))
+  (is (:error (parse (str test-data2 "\nasdfasdfsadf"))))
+  (is (= (:error (parse (str "sadfsadf\n" test-data2)))
+         :failed-to-parse))
+  (is (= (:error (parse (str "Kattskit:\n" test-data2)))
+         :section-not-recognized)))
